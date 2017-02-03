8418d1ffb9b22ca9a45bf8f1908ce5bcbb95773f
Sinala7 <jsinala7@gmail.com>
2017-02-03 11:24:19 -0600

    Create sys_print_other.c

---
+++ sys_print_other.c
@@ -0,0 +1,34 @@
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/module.h>
+
+asmlinkage int sys_print_other(int inputPID)
+{
+
+    struct task_struct *task;
+  
+    /* Find task by PID */
+    task = pid_task(find_vpid(inputPID), PIDTYPE_PID);
+  
+    /* Check for NULL */
+    if (task == NULL)
+    {
+    return 0;
+    }
+    
+    /* Print information for task */
+  	printk("Process ID: %d\n", task->pid);
+	printk("Running State: %ld\n", task->state);
+	printk("Program Name: %s\n", task->comm);
+	printk("Start Time: %llx\n", task->se.exec_start);
+	printk("Virtual Runtime: %llx\n", task->se.vruntime);
+
+	printk("\nPARENT PROCESSES:\n");
+	
+	for(task = task->parent; task != &init_task; task=task->parent) 
+    {
+		printk("\t%s [%d] - %ld\n", task->comm, task->pid, task->state);
+	}
+ 
+    return 1;
+}
