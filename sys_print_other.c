9fa01ca52aa684c80634b4a1e52f6ee156e29899
Sinala7 <jsinala7@gmail.com>
2017-02-03 11:28:22 -0600

    Update sys_print_other.c

--- sys_print_other.c
+++ sys_print_other.c
@@ -17,18 +17,18 @@ asmlinkage int sys_print_other(int inputPID)
     }
     
     /* Print information for task */
-	printk("Process ID: %d\n", task->pid);
-	printk("Running State: %ld\n", task->state);
-	printk("Program Name: %s\n", task->comm);
-	printk("Start Time: %llx\n", task->se.exec_start);
-	printk("Virtual Runtime: %llx\n", task->se.vruntime);
+    printk("Process ID: %d\n", task->pid);
+    printk("Running State: %ld\n", task->state);
+    printk("Program Name: %s\n", task->comm);
+    printk("Start Time: %llx\n", task->se.exec_start);
+    printk("Virtual Runtime: %llx\n", task->se.vruntime);
 
-	printk("\nPARENT PROCESSES:\n");
+    printk("\nPARENT PROCESSES:\n");
 	
-	for(task = task->parent; task != &init_task; task=task->parent) 
+    for(task = task->parent; task != &init_task; task=task->parent) 
     {
-		printk("\t%s [%d] - %ld\n", task->comm, task->pid, task->state);
-	}
+	printk("\t%s [%d] - %ld\n", task->comm, task->pid, task->state);
+    }
  
     return 1;
 }
