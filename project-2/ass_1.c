374ea229eb6572ace0d0a8ecfbe3cb6a6b19e8fb
Cameron Moreau <cameron.cm6@gmail.com>
2017-02-28 10:37:56 -0600

    Fixed the shitty copy paste format

--- project-2/ass_1.c
+++ project-2/ass_1.c
@@ -6,67 +6,67 @@
 
 int total = 0;
 int n1, n2;
-char ∗s1, ∗s2;
-FILE ∗fp;
+char *s1, *s2;
+FILE *fp;
 
 
-intreadf(FILE ∗fp)
+int readf(FILE *fp)
 {
   if((fp=fopen("strings.txt","r"))==NULL){
     printf("ERROR: can’t open string.txt!\n");
     return 0;
   }
 
-  s1 = (char∗)malloc(sizeof(char)∗MAX);
+  s1 = (char*)malloc(sizeof(char)*MAX);
   if(s1==NULL){
     printf("ERROR: Out of memory!\n");
-    return −1;
+    return -1;
   }
 
-  s2 = (char∗)malloc(sizeof(char)∗MAX);
+  s2 = (char*)malloc(sizeof(char)*MAX);
   if(s1==NULL){
     printf("ERROR: Out of memory\n");
-    return −1;
+    return -1;
   }
 
-  /∗ reads 1s 2 from the file ∗/
+  /* reads 1s 2 from the file */
   s1 = fgets(s1,MAX,fp);
   s2 = fgets(s2,MAX,fp);
-  n1 = strlen(s1); /∗ length of s1 ∗/
-  n2 = strlen(s2)−1; /∗ length of s2 ∗/
+  n1 = strlen(s1); /* length of s1 */
+  n2 = strlen(s2)-1; /* length of s2 */
 
-  if(s1==NULL||s2==NULL||n1<n2) /∗ when error exit ∗/
-    return −1;
+  if(s1==NULL||s2==NULL||n1<n2) /* when error exit */
+    return -1;
 }
 
-intnum_substring(void)
+int num_substring(void)
 {
   int i, j, k;
   int count;
 
-  for(i=0;i<=(n1−n2);i++){
-    count=0;
+  for(i=0;i<=(n1-n2);i++){
+    count = 0;
     
-    for(j=i,k=0;k<n2;j++,k++){ /∗ search for the next string of size of n 2 ∗/
-      if(∗(s1+j)!=∗(s2+k)){
+    for(j=i,k=0;k<n2;j++,k++){ /* search for the next string of size of n 2 */
+      if(*(s1+j)!=*(s2+k)){
         break;
       }
       else
         count++;
 
       if(count==n2)
-        total++; /∗ find a substring in this step ∗/
+        total++; /* find a substring in this step */
     }
   }
 
   return total;
 }
 
-int main(int argc, char ∗argv[])
+int main(int argc, char *argv[])
 {
   int count;
   readf(fp);
-  count=num_substring();
+  count = num_substring();
   printf("The number of substrings is: %d\n",count);
   return 1;
 }
